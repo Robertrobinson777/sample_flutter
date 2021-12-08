dbe513980e43c425108f52293235c5760cd05196
Robert Robinson <robert.robinson@augustahitech.com>
2021-12-08 15:57:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,144 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:sample/postModel.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  PostList? _posts;
+
+  @override
+  void initState() {
+    super.initState();
+    getList();
+  }
+
+  Future<void> getList() async {
+    final response =
+        await http.get(Uri.parse('https://reqres.in/api/users?page=1'));
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      final jsonResponse = json.decode(response.body);
+      _posts = PostList.fromJson(jsonResponse);
+      setState(() {});
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _posts == null
+                ? const CircularProgressIndicator()
+                : ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: _posts!.data!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(_posts!.data![index].firstName),
+                        subtitle: Text(_posts!.data![index].lastName),
+                      );
+                    }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/postModel.dart
@@ -0,0 +1,102 @@
+// To parse this JSON data, do
+//
+//     final postList = postListFromJson(jsonString);
+
+import 'dart:convert';
+
+PostList postListFromJson(String str) => PostList.fromJson(json.decode(str));
+
+String postListToJson(PostList data) => json.encode(data.toJson());
+
+class PostList {
+  PostList({
+    required this.page,
+    required this.perPage,
+    required this.total,
+    required this.totalPages,
+    required this.data,
+    required this.support,
+  });
+
+  int page;
+  int perPage;
+  int total;
+  int totalPages;
+  List<Datum>? data;
+  Support? support;
+
+  factory PostList.fromJson(Map<String, dynamic> json) => PostList(
+        page: json["page"] == null ? null : json["page"],
+        perPage: json["per_page"] == null ? null : json["per_page"],
+        total: json["total"] == null ? null : json["total"],
+        totalPages: json["total_pages"] == null ? null : json["total_pages"],
+        data: json["data"] == null
+            ? null
+            : List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+        support:
+            json["support"] == null ? null : Support.fromJson(json["support"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "page": page == null ? null : page,
+        "per_page": perPage == null ? null : perPage,
+        "total": total == null ? null : total,
+        "total_pages": totalPages == null ? null : totalPages,
+        "data": data == null
+            ? null
+            : List<dynamic>.from(data!.map((x) => x.toJson())),
+        "support": support == null ? null : support!.toJson(),
+      };
+}
+
+class Datum {
+  Datum({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  int id;
+  String email;
+  String firstName;
+  String lastName;
+  String avatar;
+
+  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+        id: json["id"] == null ? null : json["id"],
+        email: json["email"] == null ? null : json["email"],
+        firstName: json["first_name"] == null ? null : json["first_name"],
+        lastName: json["last_name"] == null ? null : json["last_name"],
+        avatar: json["avatar"] == null ? null : json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id == null ? null : id,
+        "email": email == null ? null : email,
+        "first_name": firstName == null ? null : firstName,
+        "last_name": lastName == null ? null : lastName,
+        "avatar": avatar == null ? null : avatar,
+      };
+}
+
+class Support {
+  Support({
+    required this.url,
+    required this.text,
+  });
+
+  String url;
+  String text;
+
+  factory Support.fromJson(Map<String, dynamic> json) => Support(
+        url: json["url"] == null ? null : json["url"],
+        text: json["text"] == null ? null : json["text"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "url": url == null ? null : url,
+        "text": text == null ? null : text,
+      };
+}
